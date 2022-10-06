6a3374a63692a2b1dc531161d7b92f651440d22f
Panagiotis <panfanartzis@gmail.com>
2022-10-07 01:25:54 +0300

    working on requests

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,48 @@
+import { useState, useEffect } from "react";
 import "./App.scss";
+import axios from "axios";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar";
-import useAxios from "./hooks/useAxios";
 
 const App = () => {
-  // const { response, error, loading } = useAxios({
-  //   url: "http://ec2-52-28-61-139.eu-central-1.compute.amazonaws.com:3000/test",
-  // });
-  const { response, error, loading } = useAxios({
-    url: "http://localhost:4000/test",
-  });
+  const [auth, setAuth] = useState(null);
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    axios
+      .request({
+        data: { email: "user6@gmail.com", password: "123" },
+        signal: controller.signal,
+        method: "POST",
+        url: "http://localhost:4000/api/users/login",
+      })
+      .then((response) => {
+        setLoading(false);
+        setResponse(response);
+        setAuth(response.data["auth-token"]);
+        console.log(response);
+      })
+      .catch((error) => {
+        if (error.message != "canceled") {
+          setLoading(false);
+          setError(error.message);
+          console.log(error);
+        }
+      });
+    return () => controller.abort();
+  }, []);
 
   return (
     <>
       <Navbar />
-      <div className="container">
-        <Home response={response} error={error} loading={loading} />
-      </div>
+      <main className="container">
+        <Home auth={auth} />
+      </main>
     </>
   );
 };
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,18 +1,69 @@
 import "./Home.scss";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import SkeletonPost from "../../skeletons/SkeletonPost/SkeletonPost";
 
-const Home = ({ response, loading, error }) => {
+const Home = ({ auth }) => {
   let numberOfSkeletonPosts = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const controller = new AbortController();
+      axios
+        .request({
+          method: "GET",
+          url: "http://localhost:4000/api/posts/",
+          headers: {
+            ["auth-token"]: auth,
+          },
+          signal: controller.signal,
+        })
+        .then((response) => {
+          setLoading(false);
+          setResponse(response);
+          setError(null);
+          console.log(response);
+        })
+        .catch((error) => {
+          if (error.message != "canceled") {
+            console.log(error);
+            setLoading(false);
+            setError(error.message);
+          }
+        });
+      return () => controller.abort();
+    }, 2000);
+  }, [auth]);
+
   return (
     <div className="home">
-      {!loading && (
+      {loading ? (
         <div className="flex">
-          <div>{response && response.data.response}</div>
-          <div>User info</div>
+          <div>
+            {numberOfSkeletonPosts.map((element) => (
+              <SkeletonPost key={element} />
+            ))}
+          </div>
+          <h1>test</h1>
+        </div>
+      ) : (
+        <div className="flex">
+          <div>
+            {response &&
+              response.data.map((item) => (
+                <div key={item._id}>
+                  <h3>{item.title}</h3>
+                  <p>{item.content}</p>
+                </div>
+              ))}
+          </div>
+          <h1>User info</h1>
         </div>
       )}
-      {loading && numberOfSkeletonPosts.map((element) => <SkeletonPost />)}
-      {error}
     </div>
   );
 };
