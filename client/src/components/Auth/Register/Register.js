df5428f217b0aeae151f5916ea62cc51926a5d86
Panagiotis <panfanartzis@gmail.com>
2022-10-09 16:45:26 +0300

    ~~

--- client/src/components/Auth/Register/Register.js
+++ client/src/components/Auth/Register/Register.js
@@ -1,7 +1,14 @@
 import CustomToast from "../CustomToast";
+import { useState } from "react";
 import "./Register.scss";
 
 const Register = ({ auth, setAuth }) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [name, setName] = useState(null);
+  const [password, setPassword] = useState(null);
+
   return (
     <section className="register container">
       <p>Register page</p>
