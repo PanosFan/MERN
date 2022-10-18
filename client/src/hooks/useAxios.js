b658096f95bbb21f6958e7f275727b0168e53abb
Panagiotis <panfanartzis@gmail.com>
2022-10-18 23:11:38 +0300

    ~~

--- client/src/hooks/useAxios.js
+++ client/src/hooks/useAxios.js
@@ -6,28 +6,29 @@ const useAxios = ({ method = "GET", url, auth }) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const controller = new AbortController();
+    setTimeout(() => {
+      const controller = new AbortController();
+      axios
+        .request({
+          method,
+          url,
+          signal: controller.signal,
+          headers: {
+            "auth-token": auth,
+          },
+        })
+        .then((response) => {
+          setResponse(response);
+          console.log(response.data);
+        })
+        .catch((error) => {
+          if (error.message !== "canceled") {
+            setError(error.message);
+          }
+        });
 
-    axios
-      .request({
-        method,
-        url,
-        signal: controller.signal,
-        headers: {
-          "auth-token": auth,
-        },
-      })
-      .then((response) => {
-        setResponse(response);
-        console.log(response.data);
-      })
-      .catch((error) => {
-        if (error.message !== "canceled") {
-          setError(error.message);
-        }
-      });
-
-    return () => controller.abort();
+      return () => controller.abort();
+    }, 1500);
   }, []);
   return { response, error };
 };
